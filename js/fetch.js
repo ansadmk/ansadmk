5d40f3d1f6fd7859f716ea2e753b4a7563896385
Mohemed ansad mk <92428126+ansadmk@users.noreply.github.com>
2023-05-24 07:20:10 +0530

    Update fetch.js

--- js/fetch.js
+++ js/fetch.js
@@ -2,7 +2,7 @@
 
 const fetchsend=function(str){
      
-     fetch (`../pages/${str}.html`)
+     fetch (`./pages/${str}.html`)
     .then(x => x.text())
     .then(y => document.getElementById("container").innerHTML = y);
 }
